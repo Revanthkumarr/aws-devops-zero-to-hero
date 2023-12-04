5542c803c3671c2d72febcdc8fc0f57c60c24480
Revanthkumarr <51317998+Revanthkumarr@users.noreply.github.com>
2023-12-03 22:34:07 -0600

    added docker files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull samuji/simple_flask:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 samuji/simple_flask:tagname
