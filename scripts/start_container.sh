853efa3de60f52297ffbd21f4e4890e5802933f7
Revanthkumarr <51317998+Revanthkumarr@users.noreply.github.com>
2023-12-03 22:57:47 -0600

    add

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull samuji/simple_flask:latest
+docker pull samuji/simple_flask:latest
 
 # Run the Docker image as a container
 sudo docker run -d -p 5000:5000 samuji/simple_flask:latest
